6adcbe0e390f75d67429663adab247be27f0b08e
theodorpanguripan <166774123+theodorpanguripan@users.noreply.github.com>
2024-09-16 15:37:27 +0800

    first commit

--- config/awsBucketConfig.js
+++ config/awsBucketConfig.js
@@ -2,7 +2,7 @@ const { S3Client } = require("@aws-sdk/client-s3");
 const s3Client = new S3Client({
   region: "ap-southeast-1",
   credentials: {
-    accessKeyId: "AKIAQNEKXBK6ZNI4MBXQ",
+    accessKeyId: process.env.AWS_ACCESS_ID,
     secretAccessKey: process.env.AWS_SECRET_KEY,
   },
 });
